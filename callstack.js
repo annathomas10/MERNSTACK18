3ccf62f367ffcae3807d06b1a742f463e8c767b6
anna thomas <zarafa000@yahoo.com>
2024-04-06 14:03:11 -0400

    comments

--- callstack.js
+++ callstack.js
@@ -21,3 +21,17 @@ function User(params) {
 }
 
 User()
+
+/* good article - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Event_loop
+"A downside of this model is that if a message takes too long to complete, the web 
+application is unable to process user interactions like click or scroll. "
+setTimeout:
+" For this reason, the second argument indicates a minimum time — not a guaranteed time."
+"Zero delay doesn't mean the call back will fire-off after zero milliseconds"
+"The execution depends on the number of waiting tasks in the queue"
+"A very interesting property of the event loop model is that JavaScript, 
+unlike a lot of other languages, never blocks"
+"Legacy exceptions exist like alert or synchronous XHR, but it is considered good practice
+to avoid them. Beware: exceptions to the exception do exist (but are usually implementation 
+bugs, rather than anything else)."
+Also see his slides */
