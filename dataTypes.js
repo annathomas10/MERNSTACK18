d2e13c8fc8b5d5e75ae0b062970d68345ab95c2b
anna thomas <zarafa000@yahoo.com>
2024-04-06 14:30:24 -0400

    comments

--- dataTypes.js
+++ dataTypes.js
@@ -7,14 +7,14 @@
 
 //console.log(data_name1) //data_name1 is not defined : error
 
-console.log(data_name) // undefined - hoisting - (default value to variable in js)
+console.log('1 - ', data_name) // undefined - hoisting - (default value to variable in js)
 
 //var - is a keyword used in core-js to declare variables, function etc
 
 var data_name = "Learning MERNStack" //declare and assign value to the variable
 
-console.log(data_name)
-console.log("Data Type ", typeof data_name) //data type of data - string
+console.log('2 - ', data_name)
+console.log("3 - Data Type ", typeof data_name) //data type of data - string
 
 // var data_name = "Learning Vanila Javascript" //re-declare and re-assign value to the variable
 // data_name = "Learning Ecma Script basics" // re-assign value to the variable
@@ -22,18 +22,18 @@ console.log("Data Type ", typeof data_name) //data type of data - string
 var data_name = 2024  //dynamically changes the type depending on the value assigned
 data_name = 20.24 //float
 
-console.log(data_name)
-console.log("Data Type ", typeof data_name) //data type of data - number
+console.log('4 - ',data_name)
+console.log("5 - Data Type ", typeof data_name) //data type of data - number
 
 var data_name = undefined
 
 console.log(data_name)
-console.log("Data Type ", typeof data_name) //data type of data - undefined
+console.log("6 - Data Type ", typeof data_name) //data type of data - undefined
 
 data_name = null //data value
 
 console.log(data_name)
-console.log("Data Type ", typeof data_name) //data type of data - object
+console.log("7 - Data Type ", typeof data_name) //data type of data - object
 
 data_name = {} //creating a object, json object, object literal
 
@@ -43,12 +43,12 @@ data_name = {
 }
 
 console.log(data_name)
-console.log("Data Type ", typeof data_name) //data type of data - object
+console.log("8 - Data Type ", typeof data_name) //data type of data - object
 
 data_name = null
 
 console.log(data_name)
-console.log("Data Type ", typeof data_name) 
+console.log("9 - Data Type ", typeof data_name) //this is 'object', thought it would be null
 
 data_name = {
     FirstName : "Mark", 
@@ -58,16 +58,16 @@ data_name = {
 data_name = 1 == 2//true // default value for bool is false
 
 console.log(data_name)
-console.log("Data Type ", typeof data_name) 
+console.log("10 - Data Type ", typeof data_name) 
 
 //empty object how js see this
 
 var emptyObj =  {} //obj1 // 213-address
 var emptyObj2 = {} //obj1 // 214-address
 
-data_name = emptyObj == emptyObj2 // ==> true 
+data_name = emptyObj == emptyObj2 // ==> this is false. Thought it would be true
 
-console.log(data_name)
+console.log('11 - ',data_name)
 
 
 
@@ -77,4 +77,4 @@ var symbol_1 = Symbol("New Value of my choice")
 
 
 console.log(symbol_1)
-console.log("Data Type ", typeof symbol_1) 
+console.log("12 - Data Type ", typeof symbol_1) 
