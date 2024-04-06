baf61d29cd58d687c942ab55384a573425bbc575
anna thomas <zarafa000@yahoo.com>
2024-04-06 15:15:13 -0400

    comments

--- functionAsClass.js
+++ functionAsClass.js
@@ -17,6 +17,8 @@ var funcAsClass = function(name, age, address){
     }
 }
 
+//session was not part of the ctor function. To add, must add to its prototype (which is js object...but in chrome
+//browser I see it as part of funcAsClass. Bit odd, but thats ok.....)
 funcAsClass.prototype.session = "Javsacript Basics!!!";
 
 var funcObj = new funcAsClass("Bryan", 20, "US")
@@ -27,12 +29,12 @@ console.log(funcObj.getUserDetails(true))
 var objOfFuncConstructor = new funcAsClass("Arda", 19.6, "Somewhere on earth")
 
 //address does print ......... so its notg private??? What about closure - if I remove "this", it becomes 
-//undefined. "this" gave the object instances access
+//undefined. "this" gave the object instances access. Without that, it was no longer "private"
 console.log(objOfFuncConstructor.address);  
 console.log(objOfFuncConstructor.getUserDetails())
 
 //using protoype we can add properties in child class or object
-//objOfFuncConstructor.__proto__.hobbies
+//objOfFuncConstructor.__proto__.hobbies ---------- can use this too. Same effect. No difference........
 objOfFuncConstructor.hobbies = "Reading, Playing Soccer";
 
 objOfFuncConstructor.getHobbies = function (params) {
