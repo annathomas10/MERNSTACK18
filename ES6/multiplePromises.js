6c7f0c649d9a2095a13516ccc8ca2d0f53c88216
anna thomas <zarafa000@yahoo.com>
2024-04-22 13:28:45 -0400

    comment

--- ES6/multiplePromises.js
+++ ES6/multiplePromises.js
@@ -1,3 +1,7 @@
+
+//The promise.js has the use of promise for flowing from one call to the other
+//The login proces uses authentication folllowed by authorization
+
 //Create a Promise
 const myPromise1 = new Promise((resolve, reject) => {
   setTimeout(resolve, 2000, "King");
@@ -14,6 +18,9 @@ const myPromise2 = new Promise((resolve, reject) => {
 //eg use case for the 2 uses below : a website with 3 panels. But want to display
 //only if all pass etc. His code is diff a bit
 
+//Think of the promise api sending the call to the event loop since it is 
+//the one who actually holds the call
+
 // All have to pass or all fail
 /*Promise.all([myPromise1, myPromise2]).then((x) => {
   console.log('all done')
@@ -21,7 +28,8 @@ const myPromise2 = new Promise((resolve, reject) => {
     console.log("execution is failed  ", response) //rejected
 })*/
 
-//even if one passes, Promise will say "all is good"
+//even if one passes, Promise will say "all is good". The failed one i can reinitiate
+//or send dummy data etc
 Promise.allSettled([myPromise1, myPromise2]).then((x) => {
     console.log('all done')
   }).catch((response, error)=>{
